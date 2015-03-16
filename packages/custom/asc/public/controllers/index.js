4277320f6f4a68fc96e8f82209d387d7102edb25
awang4 <awang4@ncsu.edu>
2015-03-15 22:22:44 -0400

    Layout work

--- packages/custom/asc/public/controllers/index.js
+++ packages/custom/asc/public/controllers/index.js
@@ -5,9 +5,9 @@ angular.module('mean.asc').controller('IndexController', ['$scope', 'Global',
     $scope.global = Global;
     $scope.bannerInterval = 5000;
     $scope.bannerImgs = [ 
-			{ img: '/asc/assets/img/1553474_10152659059026720_8478869418720111552_o.jpg' },
-			{ img: '/asc/assets/img/10960113_10152659057806720_7158304801537669256_o.jpg' },
-			{ img: '/asc/assets/img/10557085_10152659058786720_2725854966728482093_o.jpg' }
+			{ img: '/asc/assets/img/bannerImg0.jpg' },
+			{ img: '/asc/assets/img/bannerImg1.jpg' },
+			{ img: '/asc/assets/img/bannerImg2.jpg' }
 		];
 	}
 ]);
