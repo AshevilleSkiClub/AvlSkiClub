cb293d18bb6576f5e6d403c51c42441048f19520
awang4 <awang4@ncsu.edu>
2015-07-14 20:39:31 -0400

    Added schedule. Moved sidebar

--- app/racing/racing.js
+++ app/racing/racing.js
@@ -31,5 +31,19 @@ angular.module('myApp.racing', ['ngRoute'])
     onLoad();
 }])
 
-
+.controller('RacingScheduleCtrl', ['$scope', function($scope) {
+    $scope.schedule = [
+        { date: 'December 19 - 20', place: 'Appalachian Mt, NC', time: '9:30 am'},
+        { date: 'January 9 - 10', place: 'Sugar Mt, NC', time: '9:00 am'},
+        { date: 'January 16 - 23', place: 'Crescent Ski Council Ski Week', time: ''},
+        { date: 'January 30 - 31', place: 'Appalachian Mt, NC', time: '9:30 am'},
+        { date: 'February 1', place: 'Cupp Run Challenge at Snowshoe, WV', time: ''},
+        { date: 'February 6 - 7', place: 'Beech Mt, NC', time: '10:00 am'},
+        { date: 'February 20 - 21', place: 'Sugar Mt, NC', time: '9:00 am'},
+        { date: 'March TBA', place: 'Governor’s Cup Race at Timberline, WV', time: ''},
+        { date: 'March TBA', place: 'Dick Trundy/Sugar Cup GS Sugar Mt', time: '9:00 am'},
+        { date: 'March 4 - 6', place: 'Crescent Cup, SilverCreek, WV', time: '8:45 am'},
+        { date: 'March TBA', place: 'Rich Mead', time: ''},
+    ];
+}])
 ;
