1adcb474f6354a0302ef5cfe09a11edc5dcfa8e5
awang4 <awang4@ncsu.edu>
2015-10-16 19:01:20 -0400

    event updates

--- app/home/home.js
+++ app/home/home.js
@@ -11,22 +11,31 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
-        topic: 'Bye-bye Thursday Meetings & By-law Changes', 
-        location: 'Avenue M', 
-        address: '791 Merrimon Ave Asheville, NC 28804', 
-        mapUrl: 'https://www.google.com/maps/place/Avenue+M/@35.625743,-82.553496,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x6c37d17d9cf1317d',
-        date: 'Thursday, October 8',
-        time: '6:30 pm',
-        notes: 'Appetizers will be served'
+        topic: 'Discount shopping night and mini fashion show!', 
+        location: 'Ski Country Sports', 
+        address: '1000 Merrimon Ave Asheville, NC 28804', 
+        mapUrl: 'https://www.google.com/maps?q=ski+country+sports+asheville&rlz=1C1ADCB_enUS568US568&ion=1&espv=2&es_th=1&bav=on.2,or.r_cp.&bvm=bv.105454873,d.dmo&biw=1920&bih=980&dpr=1&um=1&ie=UTF-8&sa=X&ved=0CAcQ_AUoAmoVChMIosjFk4HIyAIVyzweCh3wyQ2U',
+        date: 'Tuesday, November 10',
+        time: '6:30 pm'
 	};
     
     $scope.otherAnnouncements = [
         {
-            name: 'CSC Fall Conference',
-            location: 'Glenstone Lodge',
-            address: '504 Historic Nature Trail, Gatlinburg, TN',
-            mapUrl: 'https://www.google.com/maps/place/Glenstone+Lodge/@35.7048819,-83.5176635,17z/data=!3m1!4b1!4m2!3m1!1s0x885955ce286680e3:0x8225a7395e231487',
-            date: 'October 23-25'
+            name: 'Club Social/Celebration',
+            location: 'All Souls Pizza',
+            address: '175 Clingman Ave Asheville, NC 28801',
+            mapUrl: 'https://www.google.com/maps/place/All+Souls+Pizza/@35.5879628,-82.566547,17z/data=!3m1!4b1!4m2!3m1!1s0x88598cad7e57f9a7:0xc6c2d37cece59fb7',
+            date: 'October 18',
+            time: '4:00 pm'
+        },
+        {
+            name: 'Warren Miller\'s Chasing Shadows',
+            location: 'New Mountain Asheville',
+            address: '38 N French Broad Ave Asheville, NC 28801',
+            mapUrl: 'https://www.google.com/maps/place/New+Mountain+Asheville/@35.5945235,-82.5613704,17z/data=!3m1!4b1!4m2!3m1!1s0x8859f354f5898bcb:0xa0c6cc1c341081f3',
+            date: 'November 13',
+            time: '5:00 pm',
+            notes: 'Tickets: $10'
         }
     ];
     
