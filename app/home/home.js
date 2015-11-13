843c3b8143dc0146b80c78c472dbd69e9dd304aa
awang4 <awang4@ncsu.edu>
2015-11-13 00:14:27 -0500

    News updates

--- app/home/home.js
+++ app/home/home.js
@@ -11,11 +11,11 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
-        topic: 'Discount shopping night and mini fashion show!', 
-        location: 'Ski Country Sports', 
-        address: '1000 Merrimon Ave Asheville, NC 28804', 
-        mapUrl: 'https://www.google.com/maps?q=ski+country+sports+asheville&rlz=1C1ADCB_enUS568US568&ion=1&espv=2&es_th=1&bav=on.2,or.r_cp.&bvm=bv.105454873,d.dmo&biw=1920&bih=980&dpr=1&um=1&ie=UTF-8&sa=X&ved=0CAcQ_AUoAmoVChMIosjFk4HIyAIVyzweCh3wyQ2U',
-        date: 'Tuesday, November 10',
+        topic: 'Holiday party with silent auction', 
+        location: 'Southern Kitchen & Bar', 
+        address: '41 N Lexington Ave Asheville, NC 28801', 
+        mapUrl: 'https://www.google.com/maps/place/The+Southern+Kitchen+And+Bar/@35.5962333,-82.5556647,17z/data=!3m1!4b1!4m2!3m1!1s0x8859f357b3fdd449:0xd0de41a5c65a6e86',
+        date: 'Tuesday, December 1',
         time: '6:30 pm'
 	};
     
@@ -27,7 +27,16 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
             mapUrl: 'https://www.google.com/maps/place/New+Mountain+Asheville/@35.5945235,-82.5613704,17z/data=!3m1!4b1!4m2!3m1!1s0x8859f354f5898bcb:0xa0c6cc1c341081f3',
             date: 'November 13',
             time: 'Doors: 5:00 pm / Part one: 7:15-8:00 pm / Part two: 8:15-9:00 pm',
-            notes: 'Tickets: $10'
+            notes: 'Tickets: Sold out!'
+        },
+        {
+            name: 'Ullr Fest',
+            location: 'Ashley Woods Park/Pavilion',
+            address: 'Ashley Woods Dr Arden, NC 28704',
+            mapUrl: 'https://www.google.com/maps/place/Ashley+Woods+Dr,+Arden,+NC+28704/@35.4841305,-82.5870739,17z/data=!3m1!4b1!4m2!3m1!1s0x885992e9c6b7965f:0xd968443366d43ac2',
+            date: 'November 14',
+            time: '5:00-9:00 pm',
+            notes: 'See newsletter for more details'
         }
     ];
     
