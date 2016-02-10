f3e593e6d938bc6dd6e1e9f6ac9053a542294384
awang4 <awang4@ncsu.edu>
2016-02-09 21:38:52 -0500

    Site updates

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -14,11 +14,11 @@ angular.module('asc.calendar', ['ngRoute'])
 
     .controller('CalendarCtrl', ['$scope', function($scope) {
         $scope.upcomingEvents = [
-            { name: 'January Meeting', date: '01/12' },
-            { name: 'Sugar Mtn Race', date: '01/09-01/10' },
-            { name: 'Mt Bachelor Trip', date: '01/16-01/23' },
-            { name: 'Courmayeur Trip', date: '01/23-01/31' },
-            { name: 'Appalachian Mtn Race', date: '01/30-01/31' }
+            { name: 'Whistler/Blackcomb Trip', date: '02/20-02/27' },
+            { name: 'Sugar Mtn Race', date: '02/20-02/21' },
+            { name: 'Crescent Cup', date: '03/04-03/06' },
+            { name: 'March Meeting', date: '03/08' },
+            { name: 'Crescent Big Sky Trip', date: '03/12-03/19' }
         ];
         
     }]);
--- app/home/home.js
+++ app/home/home.js
@@ -11,11 +11,11 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
-        topic: 'Trips and Travel with Jan Van Dine', 
+        topic: 'Trips Review', 
         location: 'Twisted Laurel', 
         address: '130 College St Asheville, NC 28801', 
         mapUrl: 'https://www.google.com/maps/place/Twisted+Laurel/@35.5961017,-82.5523825,17z/data=!3m1!4b1!4m2!3m1!1s0x8859f3583c9e03d5:0x2f6c367903d5d1f6',
-        date: 'Tuesday, January 12',
+        date: 'Tuesday, March 8',
         time: '6:30 pm Social, 7:00 pm Meeting'
 	};
     
@@ -25,25 +25,25 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 //            location: 'Ashley Woods Park/Pavilion',
 //            address: 'Ashley Woods Dr Arden, NC 28704',
 //            mapUrl: 'https://www.google.com/maps/place/Ashley+Woods+Dr,+Arden,+NC+28704/@35.4841305,-82.5870739,17z/data=!3m1!4b1!4m2!3m1!1s0x885992e9c6b7965f:0xd968443366d43ac2',
-            date: 'January 9th - 10th'
+            date: 'Februayr 20th - 21st'
 //            time: '5:00-9:00 pm',
 //            notes: 'See newsletter for more details'
         },
         {
-            name: 'Mt Bachelor',
+            name: 'Whistler/Blackcomb',
 //            location: 'Ashley Woods Park/Pavilion',
 //            address: 'Ashley Woods Dr Arden, NC 28704',
 //            mapUrl: 'https://www.google.com/maps/place/Ashley+Woods+Dr,+Arden,+NC+28704/@35.4841305,-82.5870739,17z/data=!3m1!4b1!4m2!3m1!1s0x885992e9c6b7965f:0xd968443366d43ac2',
-            date: 'January 16th - 23rd'
+            date: 'February 20th - 27th'
 //            time: '5:00-9:00 pm',
 //            notes: 'See newsletter for more details'
         },
         {
-            name: 'Courmayeur',
+            name: 'Crescent Cup',
 //            location: 'Ashley Woods Park/Pavilion',
 //            address: 'Ashley Woods Dr Arden, NC 28704',
 //            mapUrl: 'https://www.google.com/maps/place/Ashley+Woods+Dr,+Arden,+NC+28704/@35.4841305,-82.5870739,17z/data=!3m1!4b1!4m2!3m1!1s0x885992e9c6b7965f:0xd968443366d43ac2',
-            date: 'January 23rd - 31st'
+            date: 'March 4th - 6th'
 //            time: '5:00-9:00 pm',
 //            notes: 'See newsletter for more details'
         },
