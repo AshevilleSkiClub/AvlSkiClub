58b9101e9fd0ad643938edd03a215807bed01c12
awang4 <awang4@ncsu.edu>
2016-04-03 22:32:58 -0400

    Updates

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -14,10 +14,6 @@ angular.module('asc.calendar', ['ngRoute'])
 
     .controller('CalendarCtrl', ['$scope', function($scope) {
         $scope.upcomingEvents = [
-            { name: 'Whistler/Blackcomb Trip', date: '02/20-02/27' },
-            { name: 'Crescent Cup', date: '03/04-03/06' },
-            { name: 'March Meeting', date: '03/08' },
-            { name: 'Crescent Big Sky Trip', date: '03/12-03/19' },
             { name: 'April Meeting', date: '04/12' }
         ];
         
