8c60a3cc93618898e5b4591311ed1068c6e2d77b
awang4 <awang4@ncsu.edu>
2016-05-15 17:29:29 -0400

    St Moritz trip info

--- app/trips/trips.js
+++ app/trips/trips.js
@@ -12,8 +12,8 @@ angular.module('asc.trips', ['ngRoute'])
 .controller('TripsCtrl', ['$scope', function($scope) {
     $scope.trips = [
 //        { name: 'Mt Bachelor', date: 'Jan 16-23, 2016', templatePath: 'trips/articles/mt-bachelor.html' },
-        { name: 'Beaver Creek', date: 'Jan 14-21, 2017', templatePath: 'trips/articles/beaver-creek.html' }
-//        { name: 'Whistler-Blackcomb, BC', date: 'Feb 20-27, 2016', templatePath: 'trips/articles/whistler-blackcomb.html' },
+        { name: 'Beaver Creek', date: 'Jan 14-21, 2017', templatePath: 'trips/articles/beaver-creek.html' },
+        { name: 'St. Moritz, Switzerland', date: 'Jan 28-Feb 5, 2017', templatePath: 'trips/articles/st-moritz.html' }
 //        { name: 'Big Sky', date: 'Mar 12-19, 2016', templatePath: 'trips/articles/big-sky.html' }
     ];
     
