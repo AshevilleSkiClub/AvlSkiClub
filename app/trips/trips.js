e3435c1280b3fdb584cd23298aa3e2673e13a50c
awang4 <awang4@ncsu.edu>
2016-05-15 17:58:16 -0400

    Steamboat trip info

--- app/trips/trips.js
+++ app/trips/trips.js
@@ -11,7 +11,7 @@ angular.module('asc.trips', ['ngRoute'])
 
 .controller('TripsCtrl', ['$scope', function($scope) {
     $scope.trips = [
-//        { name: 'Mt Bachelor', date: 'Jan 16-23, 2016', templatePath: 'trips/articles/mt-bachelor.html' },
+        { name: 'Steamboat', date: 'Dec 3-10, 2016', templatePath: 'trips/articles/steamboat.html' },
         { name: 'Beaver Creek', date: 'Jan 14-21, 2017', templatePath: 'trips/articles/beaver-creek.html' },
         { name: 'St. Moritz, Switzerland', date: 'Jan 28-Feb 5, 2017', templatePath: 'trips/articles/st-moritz.html' }
 //        { name: 'Big Sky', date: 'Mar 12-19, 2016', templatePath: 'trips/articles/big-sky.html' }
