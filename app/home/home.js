c14d15b633ec8ab37ea69c1c2f4ca2f901bf8d83
awang4 <awang4@ncsu.edu>
2016-07-11 00:36:09 -0400

    Site updates

--- app/home/home.js
+++ app/home/home.js
@@ -11,24 +11,33 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
-        topic: 'Day and overnight backpack demonstration', 
-        location: 'Standard Pizza Co', 
-        address: '755 Biltmore Ave Asheville, NC 28803', 
-        mapUrl: 'https://www.google.com/maps/place/Standard+Pizza/@35.570631,-82.5473468,17z/data=!3m1!4b1!4m5!3m4!1s0x8859f33eaed33c23:0x31941cada46c13d8!8m2!3d35.570631!4d-82.5451581',
-        date: 'Tuesday, June 14',
-        time: '6:30 pm social/food/fun'
+        topic: 'TBD', 
+        location: 'Pack\'s Tavern', 
+        address: '20 S Spruce St Asheville, NC 28801', 
+        mapUrl: 'https://www.google.com/maps/place/Pack\'s+Tavern/@35.595082,-82.5517567,17z/data=!3m1!4b1!4m5!3m4!1s0x8859f3584a97e195:0x5b32226bf259602d!8m2!3d35.595082!4d-82.549568',
+        date: 'Tuesday, July 12',
+        time: '6:30 pm social, 7:00 pm meeting'
 	};
     
     $scope.otherAnnouncements = [
-//        {
-//            name: 'Sugar Mtn Race',
-//            location: 'Ashley Woods Park/Pavilion',
-//            address: 'Ashley Woods Dr Arden, NC 28704',
-//            mapUrl: 'https://www.google.com/maps/place/Ashley+Woods+Dr,+Arden,+NC+28704/@35.4841305,-82.5870739,17z/data=!3m1!4b1!4m2!3m1!1s0x885992e9c6b7965f:0xd968443366d43ac2',
-//            date: 'Februayr 20th - 21st'
-//            time: '5:00-9:00 pm',
-//            notes: 'See newsletter for more details'
-//        }
+        {
+            name: 'Asheville Pub Crawl',
+            location: 'Downtown Asheville',
+            address: 'Starts at Green Man Brewery Tasting Room',
+            mapUrl: 'https://www.google.com/maps?rlz=1C1ADCB_enUS568US568&biw=1920&bih=975&q=green+man+brewery&bav=on.2,or.r_cp.&um=1&ie=UTF-8&sa=X&ved=0ahUKEwiRouq8yurNAhWBQSYKHazdCC0Q_AUIBigB',
+            date: 'Friday, July 29',
+            time: 'Starts at 4',
+            notes: ''
+        },
+        {
+            name: 'Camping with ETMM',
+            location: 'Little Oak on South Holston Lake',
+            address: '1051 Little Oak Rd Bristol, TN 37620',
+            mapUrl: 'https://www.google.com/maps/place/Little+Oak+Campground+Host/@36.515222,-82.0508057,17z/data=!3m1!4b1!4m5!3m4!1s0x88507aa849d39ab1:0xd9c4e3002259ddf7!8m2!3d36.515222!4d-82.048617',
+            date: 'August 19-21',
+            time: '',
+            notes: '4 spots available. Contact Jeff Kenrick'
+        }
     ];
     
     $scope.slideInterval = 5000;
