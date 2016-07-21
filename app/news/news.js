47419055b77cff8c8459c025028441cdca38a1f3
awang4 <awang4@ncsu.edu>
2016-07-20 23:18:29 -0400

    Newsletter july

--- app/news/news.js
+++ app/news/news.js
@@ -15,6 +15,7 @@ angular.module('asc.news', ['ngRoute'])
     ];
     
     $scope.ascNewsletters = [
+        { name: 'July 2016', src: 'newsletters/2016/ASC_July_2016.pdf' },
         { name: 'June 2016', src: 'newsletters/2016/ASC_Jun_2016.pdf' },
         { name: 'May 2016', src: 'newsletters/2016/ASC_May_2016.pdf' },
         { name: 'April 2016', src: 'newsletters/2016/ASC_Apr_2016.pdf' },
