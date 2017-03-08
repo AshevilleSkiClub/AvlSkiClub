a4fa5d65bb15a94855e90e9c91f42e785acbc4d6
awang4 <awang4@ncsu.edu>
2017-03-07 21:38:29 -0500

    news updates

--- app/home/home.js
+++ app/home/home.js
@@ -11,26 +11,16 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
-        topic: 'Trip Reviews for Steamboat, Beaver Creek, and St. Moritz', 
+        topic: 'TBD', 
         location: 'Twisted Laurel', 
         address: '130 College St, Asheville, NC 28801', 
         mapUrl: 'https://goo.gl/maps/Cj5obZ5swpp',
-        date: 'Tuesday, February 14',
+        date: 'Tuesday, March 14',
         time: '6:30 pm social, 7:00 pm meeting/program',
-        notes: 'Appetizers provided. Bring drinks and meals.'
+        notes: ''
 	};
     
-    $scope.otherAnnouncements = [
-        {
-            name: 'Race',
-            location: 'Sugar Mountain',
-            address: '1009 Sugar Mountain Dr, Sugar Mountain, NC 28604',
-            mapUrl: 'https://goo.gl/maps/ohzhjFX14Zo',
-            date: 'February 25-26',
-            time: '',
-            notes: ''
-        }
-    ];
+    $scope.otherAnnouncements = [];
     
     $scope.slideInterval = 5000;
 	$scope.welcomeSlides = [ 
--- app/news/news.js
+++ app/news/news.js
@@ -15,6 +15,7 @@ angular.module('asc.news', ['ngRoute'])
     ];
     
     $scope.ascNewsletters = [
+        { name: 'March 2017', src: 'newsletters/2017/ASC_Mar_2017.pdf' },
         { name: 'February 2017', src: 'newsletters/2017/ASC_Feb_2017.pdf' },
         { name: 'January 2017', src: 'newsletters/2017/ASC_Jan_2017.pdf' },
         { name: 'December 2016', src: 'newsletters/2016/ASC_Dec_2016.pdf' },
