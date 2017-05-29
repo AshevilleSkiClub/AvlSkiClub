018cce808e28ab4a0f90de3d63f969f73ca117c9
awang4 <awang4@ncsu.edu>
2017-05-29 15:48:15 -0400

    trip changes

--- app/trips/trips.js
+++ app/trips/trips.js
@@ -11,9 +11,13 @@ angular.module('asc.trips', ['ngRoute'])
 
 .controller('TripsCtrl', ['$scope', function($scope) {
     $scope.trips = [
+        { name: 'Race Training Camp', date: 'June 24 – July 23, 2017', templatePath: 'trips/articles/asc-rdt.html' },
+        { name: 'Steamboat', date: 'December 2 - 9, 2017', templatePath: 'trips/articles/steamboat.html' },
+        { name: 'Mammoth', date: 'January 13 - 20, 2018', templatePath: 'trips/articles/mammoth.html' },
         { name: 'Val Gardena - Dolomites', date: 'January 26 - February 6, 2018', templatePath: 'trips/articles/val-gardena.html' },
         { name: 'Park City', date: 'February 24 - March 3, 2018', templatePath: 'trips/articles/park-city.html' },
-        { name: 'Breckenridge', date: 'April 2 - 8, 2017', templatePath: 'trips/articles/breckenridge.html' }
+        { name: 'Killington', date: 'March 18 - 23, 2018', templatePath: 'trips/articles/killington.html' },
+        { name: 'Breckenridge', date: 'April 2 - 8, 2018', templatePath: 'trips/articles/breckenridge.html' }
     ];
     
     $scope.changeSelected = function (trip) {
