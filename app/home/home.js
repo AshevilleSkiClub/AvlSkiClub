d7eb9db265456d15f925ec282a1b6de766d8b65a
awang4 <awang4@ncsu.edu>
2017-11-30 22:18:37 -0500

    site updates

--- app/home/home.js
+++ app/home/home.js
@@ -12,55 +12,36 @@ angular.module('asc.home', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 .controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.monthlyMeeting = { 
         topic: 'TBD', 
-        location: 'Ski Country Sports', 
-        address: '1000 Merrimon Ave, Asheville, NC 28804', 
-        mapUrl: 'https://goo.gl/maps/NkdbwjTorG32',
-        date: 'Tuesday, November 14',
-        time: '6:30 pm - 8:00 pm',
+        location: 'Twisted Laurel', 
+        address: '130 College St, Asheville, NC 28801', 
+        mapUrl: 'https://goo.gl/maps/eHkee6w8VbC2',
+        date: 'Tuesday, December 12',
+        time: '6:30 pm social and auction, 7:00 pm meeting',
         notes: ''
 	};
     
     $scope.otherAnnouncements = [
     { 
-        name: 'Warren Miller\'s "Line of Descent"', 
-        location: 'Fine Arts Theater', 
-        address: '36 Biltmore Ave, Asheville, NC 28801', 
-        mapUrl: 'https://goo.gl/maps/pCZbL8FCDQB2',
-        date: 'October 26',
-        time: '7:00',
-        notes: 'Tickets on sale at Fine Arts Theater or online at '
-    },
-    { 
-        name: 'Picnic at Wolf Laurel Ski Resort', 
-        location: 'Wolf Laurel Ski Resort', 
-        address: '578 Valley View Cir, Mars Hill, NC 28754', 
+        name: 'Steamboat Ski Week', 
+        location: 'Steamboat', 
+        address: '2305 Mt Werner Cir, Steamboat Springs, CO 80487', 
         mapUrl: 'https://goo.gl/maps/nhXbJJagL4G2',
-        date: 'October 28',
-        time: '11:30 - 2:00',
-        notes: 'Bring a dish to share and purchase season passes'
-    },
-    { 
-        name: 'Ullr Fest', 
-        location: 'Ashley Woods', 
-        address: 'Ashley Woods Dr, Arden, NC 28704', 
-        mapUrl: 'https://goo.gl/maps/pk93DTMmvLM2',
-        date: 'October 28',
-        time: '6:00 - 9:00',
-        notes: 'Bring a dish to share and BYOB'
-    },
-    { 
-        name: 'Caving with Ellen Hofler', 
-        location: 'Meet in Weaverville, carpooling to cave in Tennessee', 
-        address: '', 
-        mapUrl: 'https://goo.gl/maps/p5xPWg4XN1s',
-        date: 'October 29',
+        date: 'December 2 - 9',
         time: '',
-        notes: ' to rsvp'
+        notes: ''
+    },
+    { 
+        name: 'Race at Appalachian Mtn', 
+        location: 'Appalachian Ski Mtn', 
+        address: '940 Ski Mountain Rd, Blowing Rock, NC 28605', 
+        mapUrl: 'https://goo.gl/maps/RCgrKRUZYs12',
+        date: 'December 17 - 18',
+        time: '',
+        notes: ''
     }];
     
     $scope.slideInterval = 5000;
 	$scope.welcomeSlides = [ 
-        { src:'img/warrenMillerPromo.png' },
         { src:'img/welcomePic.png' },
         { src:'img/welcomePic2.png' },
         { src:'img/welcomePic3.png' },
