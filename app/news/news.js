ad4316a36f3254cc454aabe4d3ad2b7a9b77987a
awang4 <awang4@ncsu.edu>
2018-01-03 22:49:52 -0500

    news updates

--- app/news/news.js
+++ app/news/news.js
@@ -15,7 +15,7 @@ angular.module('asc.news', ['ngRoute'])
     ];
     
     $scope.ascNewsletters = [
-        { name: 'January 2018', src: 'newsletters/2017/ASC_Jan_2018.pdf' }
+        { name: 'January 2018', src: 'newsletters/2018/ASC_Jan_2018.pdf' }
     ];
     
     $scope.cscNewsletters = [
